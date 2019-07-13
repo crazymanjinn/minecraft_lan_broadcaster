894078ca83639e520a0bc3dcc37ced169c52005d
crazymanjinn <crazymanjinn@users.noreply.github.com>
2019-07-13 09:08:55 -0400

    change to exponential backoff timer

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"strconv"
 	"syscall"
 	"time"
+
+	"github.com/cenkalti/backoff"
 )
 
 var broadcastAddress = flag.String("broadcast_addr",
@@ -24,6 +26,7 @@ var address = flag.String(
 	"listening address of minecraft server (only needed for version < 1.6)",
 )
 var logTS = flag.Bool("log_ts", true, "include timestamp in log messages")
+var verbose = flag.Bool("v", false, "verbose logging")
 
 func init() {
 	flag.Parse()
@@ -48,37 +51,45 @@ func main() {
 	}
 	defer conn.Close()
 
-	msg := fmt.Sprintf("[MOTD]%s[\\MOTD][AD]%s[\\AD]", *motd, *address)
+	msg := []byte(fmt.Sprintf("[MOTD]%s[\\MOTD][AD]%s[\\AD]", *motd, *address))
 
 	broadcastDone := make(chan struct{})
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
+	backOff := backoff.NewExponentialBackOff()
+	backOff.InitialInterval = 5 * time.Second
+	backOff.Multiplier = 1.25
+	backOff.MaxInterval = 5 * time.Second
+	backOff.MaxElapsedTime = 30 * time.Second
+	ticker := backoff.NewTicker(backOff)
+
 	go func() {
-		ticker := time.Tick(5 * time.Second)
-		consecutiveFail := uint(0)
-		for {
-			select {
-			case <-ticker:
-				log.Println("tick")
-				if err != nil {
-					log.Printf("unable to broadcast msg: %s", err)
-					if consecutiveFail > 5 {
-						log.Println("unable to broadcast msg in 5 consecutive tries; exiting")
-						close(broadcastDone)
-						return
-					}
-					time.Sleep(5 << consecutiveFail * time.Second)
-					consecutiveFail += 1
-				} else {
-					consecutiveFail = 0
-				}
-			case sig := <-sigs:
-				log.Printf("exiting on signal %s\n", sig)
-				close(broadcastDone)
-				return
+		var err error
+		for c := range ticker.C {
+			if *verbose {
+				log.Printf("c: %s\n", c)
 			}
+			_, err = conn.Write(msg)
+			if err != nil {
+				log.Printf("unable to broadcast msg: %s", err)
+				continue
+			}
+			if *verbose {
+				log.Println("success; resetting backoff")
+			}
+			backOff.Reset()
 		}
+		if err != nil {
+			log.Fatalf("failed to broadcast msg within %s; exiting", backOff.MaxElapsedTime)
+		}
+		close(broadcastDone)
+	}()
+
+	go func() {
+		sig := <-sigs
+		log.Printf("exiting on signal %s\n", sig)
+		ticker.Stop()
 	}()
 
 	<-broadcastDone
